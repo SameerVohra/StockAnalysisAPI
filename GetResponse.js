215c3e0c3c1efd2b19cfe71fa8339b5f9ec553eb
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-07-18 20:28:07 +0530

    added company details

--- GetResponse.js
+++ GetResponse.js
@@ -1,21 +1,21 @@
 const cheerio = require("cheerio");
 const request = require("request");
 
-function getResponse(url){
+function getResponse(url) {
     return new Promise((resolve, reject) => {
-        request(url, function(error, response, html){
-            if(error) {
+        request(url, function(error, response, html) {
+            if (error) {
                 console.error("Error requesting data:", error);
                 reject(error);
                 return;
             }
-        
-            if(response.statusCode !== 200) {
+
+            if (response.statusCode !== 200) {
                 console.error("Failed to load page, status code:", response.statusCode);
                 reject(`Failed to load page, status code: ${response.statusCode}`);
                 return;
             }
-        
+
             try {
                 const $ = cheerio.load(html);
                 const json = [];
@@ -23,20 +23,20 @@ function getResponse(url){
                 const currPrice = [];
                 const change = [];
                 const prevPrice = [];
-        
+
                 $('table[class="dataTable"] tbody tr').each(function() {
                     const company = $(this).find('td:nth-child(1) a').text().trim();
                     const price = $(this).find('td:nth-child(3)').text().trim();
                     const changeValue = $(this).find('td:nth-child(4)').text().trim();
                     const previousPrice = $(this).find('td:nth-child(2)').text().trim();
-        
+
                     companyName.push(company);
                     currPrice.push(price);
                     change.push(changeValue);
                     prevPrice.push(previousPrice);
                 });
-        
-                for(let i = 0; i < companyName.length; i++){
+
+                for (let i = 0; i < companyName.length; i++) {
                     json.push({
                         company: companyName[i],
                         Current_Price: currPrice[i],
@@ -44,7 +44,7 @@ function getResponse(url){
                         Previous_Price: prevPrice[i]
                     });
                 }
-        
+
                 resolve(json);
             } catch (e) {
                 console.error("Error parsing HTML:", e);
--- server.js
+++ server.js
@@ -1,53 +1,66 @@
+const { getCompany } = require("./GetCompanyDetails.js");
 const { getResponse } = require("./GetResponse.js");
 const express = require("express");
 const app = express();
 
 let url;
 
-console.log("Daily Gainers: ");
-
 const port = 3000;
-
+const companydetailsurl = "https://money.rediff.com/companies";
 app.get("/daily-gainers", (req, res) => {
-  url = "http://money.rediff.com/gainers/nse/daily/groupall";
+    console.log("daily");
+    url = "http://money.rediff.com/gainers/nse/daily/groupall";
 
-  getResponse(url)
-    .then((data) => res.status(201).json({ data }))
-    .catch((error) => {
-      res.status(501).send("INTERNAL SERVER ERROR");
-    });
+    getResponse(url)
+        .then((data) => res.status(201).json({ data }))
+        .catch((error) => {
+            res.status(501).send("INTERNAL SERVER ERROR");
+        });
 });
 
 app.get("/weekly-gainers", (req, res) => {
-  url = "http://money.rediff.com/gainers/nse/weekly/groupall";
+    url = "http://money.rediff.com/gainers/nse/weekly/groupall";
 
-  getResponse(url)
-    .then((data) => res.status(201).json({ data }))
-    .catch((error) => {
-      res.status(501).send("INTERNAL SERVER ERROR");
-    });
+    getResponse(url)
+        .then((data) => res.status(201).json({ data }))
+        .catch((error) => {
+            res.status(501).send("INTERNAL SERVER ERROR");
+        });
 });
 
-app.get("/daily-losers", (req, res)=>{
+app.get("/daily-losers", (req, res) => {
     url = "http://money.rediff.com/losers/nse/daily/groupall";
 
-  getResponse(url)
-    .then((data) => res.status(201).json({ data }))
-    .catch((error) => {
-      res.status(501).send("INTERNAL SERVER ERROR");
-    });
-})
+    getResponse(url)
+        .then((data) => res.status(201).json({ data }))
+        .catch((error) => {
+            res.status(501).send("INTERNAL SERVER ERROR");
+        });
+});
 
-app.get("/weekly-losers", (req, res)=>{
+app.get("/weekly-losers", (req, res) => {
     url = "https://money.rediff.com/losers/nse/weekly/groupall";
 
-  getResponse(url)
-    .then((data) => res.status(201).json({ data }))
-    .catch((error) => {
-      res.status(501).send("INTERNAL SERVER ERROR");
-    });
-})
+    getResponse(url)
+        .then((data) => res.status(201).json({ data }))
+        .catch((error) => {
+            res.status(501).send("INTERNAL SERVER ERROR");
+        });
+});
+
+app.get("/company-details/:name", (req, res) => {
+    const compName = req.params;
+    console.log(compName.name);
+    console.log(`${companydetailsurl}/${compName.name}`)
+    const url = `${companydetailsurl}/${compName.name}`;
+    getCompany(url)
+        .then((data) => res.status(201).send({ data }))
+        .catch((error) => {
+            console.log(error);
+            res.status(501).send("Internal Server Error")
+        });
+});
 
 app.listen(port, () => {
-  console.log(`Listening to PORT: ${port}`);
+    console.log(`Listening to PORT: ${port}`);
 });
